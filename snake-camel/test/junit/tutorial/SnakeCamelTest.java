f1dfc187fff3e4ab73b1c1bbf42f05c19904f76c
g15949tn <g15949tn@s30433.tsuda.ac.jp>
2017-04-28 11:54:41 +0900

    テストケースを追加

---
+++ snake-camel/test/junit/tutorial/SnakeCamelTest.java
@@ -0,0 +1,14 @@
+package junit.tutorial;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SnakeCamelTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
